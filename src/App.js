ec64598c3236dc8f5900656a1f272c9e19203760
Hadson Palauro Sampaio <sampaiohadson1704@outlook.com>
2022-10-15 20:33:11 -0300

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [image, setImage] = useState();
+
+  useEffect(() => {
+    nextImage()
+  }, []);
+
+  function nextImage() {
+    axios.get('https://picsum.photos/300/300', { responseType: 'blob' })
+      .then(response => {
+        setImage(URL.createObjectURL(response.data))
+      })
+      .catch(error => {
+        console.log("error when getting image: " + error)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='imageContainer'>
+        <img src={image} alt='Error' />
+        <button onClick={nextImage} className="App">Next Image</button>
+      </div>
     </div>
   );
 }
